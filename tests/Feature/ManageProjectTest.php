35d8fd50f7aa12e91298bf60fa700e4663b8763a
Pedro Santos <peusantos@gmail.com>
2021-01-19 15:36:46 -0300

    App scaffold

---
+++ tests/Feature/ManageProjectTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Project;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ManageProjectTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function guest_cannot_manage_projects()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->get('/projects')->assertRedirect('login');
+        $this->get('/projects/create')->assertRedirect('login');
+        $this->post('/projects')->assertRedirect('login');
+        $this->get($project->path(), $project->toArray())->assertRedirect('login');
+    }
+
+    /**
+     * @test 
+     * */
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        $this->get('/projects/create')->assertStatus(200);
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $response = $this->post('/projects', $attributes);
+
+        $response->assertRedirect(Project::where($attributes)->first()->path());
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    /**
+     * @test
+      */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $this->signIn();
+        $project = factory('App\Project')->create();
+        $this->get($project->path())->assertStatus(403);
+    }
+
+    /**
+     * @test */
+    public function a_project_requires_a_description()
+    {
+        $this->signIn();
+        $attributes = factory('App\Project')->raw(['description' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
+}
